67606fadfdb689a67bc2af8b7e79b5ca12065b86
sanamsanam <47135375+sanamsanam@users.noreply.github.com>
2019-10-21 01:11:58 +0300

    Delete h.py

--- Code/h.py
+++
@@ -1,8 +0,0 @@
-def LongestWord(sen):
-    from string import ascii_letters, digits
-    sen = " ".join([ch for ch in sen if ch in (ascii_letters + string.digits)])
-    return sen
-
-
-# keep this function call here
-print LongestWord(raw_input())
